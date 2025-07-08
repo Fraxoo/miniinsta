0904f337b110c72710657e0476756c45e187fbbb
Fraxoo <fraxo.officiel@gmail.com>
2025-07-08 14:04:06 +0200

    v1

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+
+function lire_dossier()
+{
+    $photos = [];
+    try {
+        $photos_dir = opendir("photos");
+
+        do {
+            $file_name = readdir($photos_dir);
+            // Je n'affiche pas les fichiers cachés (commençant par un point) et les répertoires spéciaux "." et ".."
+            if ($file_name && $file_name != "." && $file_name != ".." && $file_name != "/") {
+                $photos[] = $file_name; // J'ajoute le nom du fichier à la liste
+            }
+        } while ($file_name);
+    } catch (\Throwable $th) {
+        throw $th;
+    }
+    return $photos;
+}
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<head>
+    <title>Mini-Insta</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+
+
+
+
+<body>
+
+
+<main>
+
+   
+    <div class="gallery">
+        <?php
+        $photos = lire_dossier();
+            foreach($photos as $photo): ?>
+            <div class="post">
+            <img src="photos/<?php echo $photo ?>">
+            </div>
+            <?php endforeach; ?>
+    </div>
+
+</main>
+
+<footer>
+
+    <div class="tout">
+        
+        <form method="post" action="index.php" enctype="multipart/form-data">
+
+            <img src="images/gallery.png" style="width: 60px;">
+
+            <input type="file" id="photo" name="photo" accept="image/*" style="display:none;">
+            <label for="photo">
+                <img src="images/plus.png" alt="Ajouter une photo" style="cursor:pointer; width:60px;">
+            </label>
+
+            <input id="submit"type="submit"  value="envoyer" style="display: none;">
+            <label for="submit">
+                <img src="images/arrow.png" alt="Envoyer" style="cursor: pointer;width:45px;">
+            </label>
+
+        </form>
+
+        <?php $save = $_FILES["photo"]["tmp_name"];
+        move_uploaded_file($save,"photos/".$_FILES["photo"]["name"]); ?>
+    </div>
+
+</footer>
+
+</body>
+
+
+
---
+++ upload.php
@@ -0,0 +1,27 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+
+
+<head>
+    <title>Mini-Insta_bleu</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style2.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+
+</head>
+
+<main> 
+
+    <div class="upload">
+        <p>Upload reussi !</p>  
+    </div>
+
+    <div class="retour">
+        <a href="index.php">Retour a l'acceuil</a> 
+    </div>
+
+</main>
