ea843bbe7dcf1df5cb1d9f75e4672e0153013373
Fraxoo <fraxo.officiel@gmail.com>
2025-07-09 16:08:45 +0200

    final

--- index.php
+++ index.php
@@ -1,80 +1,69 @@
 <?php
-
 function lire_dossier()
 {
     $file_names = [];
-    
-        $photos_dir = opendir("photos");
 
-        do {
-            $file_name = readdir($photos_dir);
-            // Je n'affiche pas les fichiers cachés (commençant par un point) et les répertoires spéciaux "." et ".."
-            if ($file_name && $file_name != "." && $file_name != ".." && $file_name != "/") {
-                $file_names[] = $file_name; // J'ajoute le nom du fichier à la liste
-            }
-        } while ($file_name);
-        return $file_names;
-    
+    $files_dir = opendir("photos");
+
+    do {
+        $file_name = readdir($files_dir);
+        if ($file_name && $file_name != "." && $file_name != ".." && $file_name != "/") {
+            $file_names[] = $file_name;
+        }
+    } while ($file_name);
+    return ($file_names);
 }
+
 ?>
-
-
-
-
-
 <!DOCTYPE html>
+
+
 <head>
-    <title>Mini-Insta</title>
+    <title>Mini-InstaV2</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="styles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 </head>
 
-
-
-
 <body>
 
-
-<main>
-
-   
-    <div class="gallery">
-
-        <?php 
-        $photos = lire_dossier();
-            foreach ($photos as $photo) : ?>
-        <div class="post">  
-            
-            <?php 
-                $divise = explode("-",$photo);
-                
-                $autheur = $divise[0];
-                $jour = $divise[1];
-                $mois = $divise[2];
-                $year = $divise[3];
-            ?> 
+    <main>
 
 
-            <img src="photos/<?php echo $photo ?>" alt="">
-            <p class="big"><?php echo $autheur ?></p>
-            <p class="petit"><?php echo $jour."/".$mois."/".$year ?></p>
-            
-            
-            <?php endforeach; ?>
+        <div class="gallery">
+
+            <?php
+            $photos = lire_dossier();
+            foreach ($photos as $photo):
+            ?>
+
+                <?php
+                    $divise = explode("-", $photo);
+                    $nom = $divise[0];
+                    $jour = $divise[1];
+                    $mois = $divise[2];
+                    $year = $divise[3];
+                ?>
+
+                <div class="post">
+                    <img src="photos/<?php echo $photo ?>" alt="">
+                    <div class="para">
+                        <p class="big"><?php echo $nom ?></p>
+                        <p class="petit"><?php echo $jour . "/" . $mois . "/" . $year ?></p>
+                    </div>
+
+                </div>
+
+            <?php endforeach ?>
+
+            <div class="add">
+                <a href="upload.php"><img src="images/plus.png" alt=""></a>
+            </div>
+
         </div>
-            
-        
-        <div class="send">
-            <a href="upload.php"><img src="images/plus.png" alt=""></a>
-        </div>
-    </div>
 
-</main>
+    </main>
 
 
 
 </body>
-
-
-
--- upload.php
+++ upload.php
@@ -1,62 +1,66 @@
 <?php
-
-
 ?>
+
 <!DOCTYPE html>
 
-
 <head>
-    <title>Mini-Insta_bleu</title>
+    <title>Mini-Insta</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="styles2.css">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-
 </head>
 
 <body>
 
     <main>
 
-        <div class="try">
+        <div class="all">
 
-            <form method="post" action="upload.php" enctype="multipart/form-data">
+            <div class="mini">
+                <h1>MINI Insta</h1>
+            </div>
 
-                <input type="text" name="utilisateur" placeholder="votre nom :" required>
+            <div class="upload">
 
-                <input type="file" id="photo" name="photo" accept="image/*" style="display:none;">
-                <label for="photo">
-                    <img src="images/plus.png" alt="Ajouter une photo" style="cursor:pointer; width:60px;">
-                </label>
+                <form action="upload.php" method="post" enctype="multipart/form-data">
+                    <input type="text" name="nom" placeholder="votre nom :" required>
+                    <input type="file" name="photo">
+                    <input type="submit" value="envoyer">
+                </form>
 
-                <input id="submit" type="submit" value="envoyer" style="display: none;">
-                <label for="submit">
-                    <img src="images/arrow.png" alt="Envoyer" style="cursor: pointer;width:45px;">
-                </label>
+                <?php
+                $date = date("d-m-Y");
+                $nom = $_POST["nom"];
+                $photo = $_FILES["photo"]["name"];
+                $file_name = $nom . "-" . $date . "-" . $photo;
+                $chemin_tmp = $_FILES["photo"]["tmp_name"];
+                $upload = move_uploaded_file($chemin_tmp, "photos/" . $file_name);
+                ?>
+                <?php if ($upload !== false) : ?>
+                    <p>Upload reussi!</p>
+                    <img src="photos/<?php echo $file_name ?>">
+                <?php else : ?>
+                    <p>Veuillez entrer une image</p>
+                <?php endif ?>
 
-            </form>
 
-        <?php 
-            $date = date("d-m-Y");
-            $utilisateur = $_POST["utilisateur"];
-            $photos = $_FILES["photo"]["name"];
-            $file_name = $utilisateur ."-". $date ."-" .$photos;
-            $chemin_tmp = $_FILES["photo"]["tmp_name"];
-            $upload = move_uploaded_file($chemin_tmp,"photos/".$file_name);
 
-            if($upload){
-                echo "fichier envoyer";
-            }else{
-                echo "erreur veuillez reesayer";
-            }
-        ?>
+            </div>
+
+            <div class="retour">
+                <a href="index.php">Retour a l'acceuil</a>
+            </div>
 
         </div>
 
-        <div class="retour">
-            <a href="index.php">Retour a l'acceuil</a>
-        </div>
+
+
 
     </main>
+
+
+
+
+
 
 </body>
